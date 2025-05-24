82d439b5b9b203326b48f9bdf706f35547e486e2
Himanshu Sahni 🚀 <70528665+mrhimanshusahni@users.noreply.github.com>
2025-05-24 17:49:42 +0530

    Update custom-button.tsx

--- app/components/custom-button.tsx
+++ app/components/custom-button.tsx
@@ -1,13 +1,18 @@
 // External Dependencies
 import { cva } from "class-variance-authority";
-
-// Internal Dependencies
-import { cn } from "../lib/utils";
-import Loader from "./loader";
 import React from "react";
 
+// Internal Dependencies
+import Loader from "@/components/common/loader";
+import { cn } from "@/lib/utils";
+
 interface Button extends React.ComponentProps<"button"> {
-  variant?: "solid";
+  variant?:
+    | "solid-outline-full"
+    | "solid-rounded-full"
+    | "solid-rounded"
+    | "outline-full"
+    | "outline-rounded";
   isButtonLoading?: boolean;
   buttonLoadingText?: string;
   buttonLoaderClassName?: React.ComponentProps<"svg">["className"];
@@ -17,21 +22,37 @@ interface Button extends React.ComponentProps<"button"> {
 const CustomButton = ({
   className,
   children,
-  variant,
+  variant = "solid-outline-full",
   isButtonLoading,
   buttonLoadingText,
   buttonLoaderClassName,
   buttonLoaderSpeed = "default",
+  type = "button",
+  title = buttonLoadingText ? buttonLoadingText : children?.toString(),
   ...restProps
 }: Button) => {
   return (
     <button
       className={cn(buttonVariants({ variant }), className)}
+      type={type}
+      title={title}
       {...restProps}
     >
       {isButtonLoading ? (
         <div className="flex items-center justify-between gap-x-2">
-          <Loader className={buttonLoaderClassName} speed={buttonLoaderSpeed} />
+          <Loader
+            className={cn(
+              [
+                "solid-outline-full",
+                "solid-rounded-full",
+                "solid-rounded",
+              ]?.includes(variant)
+                ? "text-white"
+                : "text-blue-500",
+              buttonLoaderClassName,
+            )}
+            speed={buttonLoaderSpeed}
+          />
           {buttonLoadingText ? <span>{buttonLoadingText}</span> : children}
         </div>
       ) : (
@@ -41,16 +62,27 @@ const CustomButton = ({
   );
 };
 
-const buttonVariants = cva("", {
-  variants: {
-    variant: {
-      solid:
-        "flex items-center justify-between rounded-full border border-blue-500 bg-blue-500 font-medium text-white shadow-lg transition-all hover:border-none hover:outline-2 hover:outline-offset-2 hover:outline-blue-500 focus:border-none focus:outline-2 focus:outline-offset-2 focus:outline-blue-500 active:border-none active:bg-blue-600 active:outline-2 active:outline-offset-2 active:outline-blue-600 disabled:border-none disabled:opacity-50 disabled:hover:cursor-not-allowed disabled:hover:outline-none disabled:active:bg-blue-500",
+const buttonVariants = cva(
+  "inline-flex items-center justify-between px-4 py-2 h-9.5 text-sm whitespace-nowrap font-medium disabled:pointer-events-none transition-all disabled:opacity-50 shadow-xs",
+  {
+    variants: {
+      variant: {
+        "solid-outline-full":
+          "rounded-full bg-blue-500 text-white hover:outline-2 hover:outline-offset-2 hover:outline-blue-500 focus:outline-2 focus:outline-offset-2 focus:outline-blue-500 active:bg-blue-600 active:outline-2 active:outline-offset-2 active:outline-blue-600 ",
+        "solid-rounded-full":
+          "rounded-full bg-blue-500 text-white hover:bg-blue-600 active:bg-blue-700 focus:outline-2 focus:outline-offset-2 focus:outline-blue-500",
+        "solid-rounded":
+          "rounded-md bg-blue-500 text-white hover:bg-blue-600 active:bg-blue-700 focus:outline-2 focus:outline-offset-2 focus:outline-blue-500",
+        "outline-full":
+          "rounded-full border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white active:bg-blue-600 focus:outline-2 focus:outline-offset-2 focus:outline-blue-500",
+        "outline-rounded":
+          "rounded-md border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white active:bg-blue-600 focus:outline-2 focus:outline-offset-2 focus:outline-blue-500",
+      },
+    },
+    defaultVariants: {
+      variant: "solid-outline-full",
     },
   },
-  defaultVariants: {
-    variant: "solid",
-  },
-});
+);
 
 export { CustomButton };
