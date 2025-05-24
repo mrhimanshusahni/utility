123564bfe57cd5064ff3e84eb735e2c2110a6c4c
Himanshu Sahni 🚀 <70528665+mrhimanshusahni@users.noreply.github.com>
2025-05-24 18:35:02 +0530

    Update custom-button.tsx

--- app/components/custom-button.tsx
+++ app/components/custom-button.tsx
@@ -68,7 +68,7 @@ const CustomButton = ({
 };
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-between px-4 py-2 h-9.5 text-sm whitespace-nowrap font-medium disabled:pointer-events-none transition-all disabled:opacity-50 shadow-xs",
+  "inline-flex items-center justify-center px-4 py-2 h-9.5 text-sm whitespace-nowrap font-medium disabled:pointer-events-none transition-all disabled:opacity-50 shadow-xs",
   {
     variants: {
       variant: {
